9081e1131bdbbd27f084c3fbcdd1e86292af6980
Alex <meosterio@yahoo.com>
2019-05-16 21:47:09 -0400

    Add files via upload

---
+++ prog1/asemblExamp.cpp
@@ -0,0 +1,284 @@
+// Copyright 2015 Jesse Kennedy
+// Licensed under GPL 3.0
+//
+// Jesse Kennedy
+// CPSC 231-001
+// Assignment 1
+// Due 02/04/2015
+//
+// Assembles a subset of JVM assembly code into Java bytecode using a
+// two-pass structure
+//
+// Assumptions about input:
+// - Input file is read from argv[1]
+// - Opcode mnemonics are lowercased
+// - Statements contain no spaces
+// - Program should be called in the form: ./program input.m > executable
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+map<string, int> table;  // Maps label to pc
+map<string, int> opcode;  // Maps mnemonic to opcode
+
+int main(int argc, char const *argv[]) {
+    opcode["iconst_m1"] = 2;
+    opcode["iconst_0"] = 3;
+    opcode["iconst_1"] = 4;
+    opcode["iconst_2"] = 5;
+    opcode["iconst_3"] = 6;
+    opcode["iconst_4"] = 7;
+    opcode["iconst_5"] = 8;
+    opcode["bipush"] = 16;
+    opcode["iload_0"] = 26;
+    opcode["iload_1"] = 27;
+    opcode["iload_2"] = 28;
+    opcode["iload_3"] = 29;
+    opcode["iload"] = 21;
+    opcode["istore_0"] = 59;
+    opcode["istore_1"] = 60;
+    opcode["istore_2"] = 61;
+    opcode["istore_3"] = 62;
+    opcode["istore"] = 54;
+    opcode["pop"] = 87;
+    opcode["swap"] = 95;
+    opcode["dup"] = 89;
+    opcode["iadd"] = 96;
+    opcode["isub"] = 100;
+    opcode["imul"] = 104;
+    opcode["idiv"] = 108;
+    opcode["irem"] = 112;
+    opcode["ineg"] = 116;
+    opcode["iinc"] = 132;
+    opcode["ifeq"] = 153;
+    opcode["ifge"] = 156;
+    opcode["ifgt"] = 157;
+    opcode["ifle"] = 158;
+    opcode["iflt"] = 155;
+    opcode["ifne"] = 154;
+    opcode["goto"] = 167;
+    opcode["jsr"] = 168;
+    opcode["ret"] = 169;
+    opcode["invokevirtual"] = 182;
+    opcode["return"] = 177;
+
+
+    ifstream fin(argv[1]);
+
+    // General parsing strategy:
+    // 1. Read tokens separated by spaces into std::string
+    // 2. Attempt to match mnemonics that don't use parentheses
+    // 3. Otherwise, sscanf the resulting string to extract the
+    //    mnemonic and characters between parentheses
+    // 4. Two-argument mnemonics are run through sscanf again
+    //    to split the comma
+
+    int pc = 0;
+    string buf;
+    while (fin >> buf) {
+        // Test for and ignore comment(`...')
+        // len( comment(`') ) == 11
+        // len( comment ) == 7
+        if (buf.substr(0, min((int)buf.length(), 7)) == "comment") {
+            // Consume tokens until a closing ') is found
+            while (buf.find("')") == string::npos) {
+                if (!fin.good()) {
+                    cerr << "Encountered an error attempting to find the" <<
+                    " end of a comment(`') token. Crashing." << endl;
+                    return 1;
+                } else {
+                    fin >> buf;
+                }
+            }
+        } else if (buf == "iconst_m1" ||
+            buf == "iconst_0" ||
+            buf == "iconst_1" ||
+            buf == "iconst_2" ||
+            buf == "iconst_3" ||
+            buf == "iconst_4" ||
+            buf == "iconst_5" ||
+            buf == "iload_0" ||
+            buf == "iload_1" ||
+            buf == "iload_2" ||
+            buf == "iload_3" ||
+            buf == "istore_0" ||
+            buf == "istore_1" ||
+            buf == "istore_2" ||
+            buf == "istore_3" ||
+            buf == "pop" ||
+            buf == "swap" ||
+            buf == "dup" ||
+            buf == "iadd" ||
+            buf == "isub" ||
+            buf == "imul" ||
+            buf == "idiv" ||
+            buf == "irem" ||
+            buf == "ineg" ||
+            buf == "ret" ||
+            buf == "ineg" ||
+            buf == "return") {
+            // No arguments
+            pc++;
+        } else {
+            // Has arguments
+            char mnemonic[32];
+            char args[32];
+
+            // %[^(] matches a string up until a ( character
+            // ( matches the ( character
+            // %[^)] matches a string up until a ) character
+            // ) matches the ) character
+            if (sscanf(buf.c_str(), "%[^(](%[^)])", mnemonic, args) != 2) {
+                cerr << "Failed to deparenthesize token: '" << buf <<
+                "'. Crashing." << endl;
+                return 1;
+            }
+
+            if (strcmp(mnemonic, "bipush") == 0 ||
+                strcmp(mnemonic, "iload") == 0 ||
+                strcmp(mnemonic, "ifeq") == 0 ||
+                strcmp(mnemonic, "ifge") == 0 ||
+                strcmp(mnemonic, "ifgt") == 0 ||
+                strcmp(mnemonic, "ifle") == 0 ||
+                strcmp(mnemonic, "iflt") == 0 ||
+                strcmp(mnemonic, "ifne") == 0 ||
+                strcmp(mnemonic, "goto") == 0 ||
+                strcmp(mnemonic, "jsr") == 0 ||
+                strcmp(mnemonic, "invokevirtual") == 0) {
+                // One argument
+                pc += 2;
+            } else if (strcmp(mnemonic, "iinc") == 0) {
+                // Two arguments
+                pc += 3;
+            } else if (strcmp(mnemonic, "label") == 0) {
+                // Label
+                if (table.find(string(args)) == table.end()) {
+                    table[string(args)] = pc;
+                } else {
+                    cerr << "Duplicate label " << args << ". Crashing." << endl;
+                    return 1;
+                }
+            } else {
+                cerr << "Bad token: '" << buf << "'. Crashing." << endl;
+                return 1;
+            }
+        }
+    }
+
+    fin.clear();                 // clear fail and eof bits
+    fin.seekg(0, std::ios::beg);  // back to the start!
+
+    pc = 0;
+    while (fin >> buf) {
+        // Test for and ignore comment(`...')
+        // len( comment(`') ) == 11
+        // len( comment ) == 7
+        if (buf.substr(0, min((int)buf.length(), 7)) == "comment") {
+            // Consume tokens until a closing ') is found
+            while (buf.find("')") == string::npos) {
+                if (!fin.good()) {
+                    cerr << "Encountered an error attempting to find the" <<
+                    " end of a comment(`') token. Crashing." << endl;
+                    return 1;
+                } else {
+                    fin >> buf;
+                }
+            }
+        } else if (buf == "iconst_m1" ||
+            buf == "iconst_0" ||
+            buf == "iconst_1" ||
+            buf == "iconst_2" ||
+            buf == "iconst_3" ||
+            buf == "iconst_4" ||
+            buf == "iconst_5" ||
+            buf == "iload_0" ||
+            buf == "iload_1" ||
+            buf == "iload_2" ||
+            buf == "iload_3" ||
+            buf == "istore_0" ||
+            buf == "istore_1" ||
+            buf == "istore_2" ||
+            buf == "istore_3" ||
+            buf == "pop" ||
+            buf == "swap" ||
+            buf == "dup" ||
+            buf == "iadd" ||
+            buf == "isub" ||
+            buf == "imul" ||
+            buf == "idiv" ||
+            buf == "irem" ||
+            buf == "ineg" ||
+            buf == "ret" ||
+            buf == "ineg" ||
+            buf == "return") {
+            // No arguments
+            cout << pc << ": " << opcode[buf] << endl;
+            pc++;
+        } else {
+            // Has arguments
+            char mnemonic[32];
+            char args[32];
+
+            // %[^(] matches a string up until a ( character
+            // ( matches the ( character
+            // %[^)] matches a string up until a ) character
+            // ) matches the ) character
+            if (sscanf(buf.c_str(), "%[^(](%[^)])", mnemonic, args) != 2) {
+                cerr << "Failed to deparenthesize token: '" << buf <<
+                    "'. Crashing." << endl;
+                return 1;
+            }
+
+            if (strcmp(mnemonic, "bipush") == 0 ||
+                strcmp(mnemonic, "invokevirtual") == 0) {
+                // One argument. Don't lookup symbol.
+                cout << pc << ": " << opcode[string(mnemonic)] << " " <<
+                    string(args) << endl;
+                pc += 2;
+            } else if (strcmp(mnemonic, "iload") == 0 ||
+                strcmp(mnemonic, "ifeq") == 0 ||
+                strcmp(mnemonic, "ifge") == 0 ||
+                strcmp(mnemonic, "ifgt") == 0 ||
+                strcmp(mnemonic, "ifle") == 0 ||
+                strcmp(mnemonic, "iflt") == 0 ||
+                strcmp(mnemonic, "ifne") == 0 ||
+                strcmp(mnemonic, "goto") == 0 ||
+                strcmp(mnemonic, "jsr") == 0) {
+                // One argument. Lookup symbol.
+                if (table.find(string(args)) == table.end()) {
+                    cerr << "Label not found: " << args <<
+                        ". Crashing." << endl;
+                    return 1;
+                } else {
+                    cout << pc << ": " << opcode[string(mnemonic)] << " " <<
+                        table[string(args)] << endl;
+                    pc += 2;
+                }
+            } else if (strcmp(mnemonic, "iinc") == 0) {
+                // Two arguments
+                char args1[32], args2[32];
+                if (sscanf(args, "%[^,],%s", args1, args2) != 2) {
+                    cerr << "Failed to separate comma token: '" << string(args)
+                        << "'. Crashing." << endl;
+                    return 1;
+                }
+                cout << pc << ": " << opcode[string(mnemonic)] << " " <<
+                    string(args1) << " " << string(args2) << endl;
+                pc += 3;
+            } else if (strcmp(mnemonic, "label") == 0) {
+                // Label. Do nothing.
+            } else {
+                cerr << "Bad token: '" << buf << "'. Crashing." << endl;
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ prog1/backup.cpp
@@ -0,0 +1,211 @@
+/* Alex Moore (alex9)
+ * CPSC 2310 Sec 001
+ * Program1
+ * 2/15/18
+ *
+ * Program is a jvm assembler which creates an executable file
+ * called "executable". Two pass structure is used.
+ */
+
+ #include <iostream>
+ #include <fstream>
+ #include <map>
+ #include <string>
+ #include <cstring>
+
+ using namespace std;
+
+ map<string, int> table;
+ map<string, int> opcode;
+
+ int main(int argc, char const *argv[]) {
+   //initialize the opcode map with the opcodes value (map int to each opcode)
+   opcode["iconst_m1"] = 2; opcode["iconst_0"] = 3; opcode["iconst_1"] = 4;
+   opcode["iconst_2"] = 5; opcode["iconst_3"] = 6; opcode["iconst_4"] = 7;
+   opcode["iconst_5"] = 8; opcode["bipush"] = 16; opcode["iload_0"] = 26;
+   opcode["iload_1"] = 27; opcode["iload_2"] = 28; opcode["iload_3"] = 29;
+   opcode["iload"] = 21; opcode["istore_0"] = 59; opcode["istore_1"] = 60;
+   opcode["istore_2"] = 61; opcode["istore_3"] = 62; opcode["istore"] = 54;
+   opcode["pop"] = 87; opcode["swap"] = 95; opcode["dup"] = 89;
+   opcode["iadd"] = 96; opcode["isub"] = 100; opcode["imul"] = 104;
+   opcode["idiv"] = 108; opcode["irem"] = 112; opcode["ineg"] = 116;
+   opcode["iinc"] = 132; opcode["ifeq"] = 153; opcode["ifge"] = 156;
+   opcode["ifgt"] = 157; opcode["ifle"] = 158; opcode["iflt"] = 155;
+   opcode["ifne"] = 154; opcode["goto"] = 167; opcode["jsr"] = 168;
+   opcode["ret"] = 169; opcode["return"] = 177; opcode["invokevirtual"] = 182;
+
+   //make sure an input file is present
+   if (argc < 2) {
+     cerr << "Improper usage!!  Exitting\n";
+     return 1;
+   }
+
+   //open the file, create an executable to print to
+   ifstream fin(argv[1]);
+   ofstream executable("testing");
+
+   //create a string to read in all the info from the file
+   //create the location counter
+   //create bool to check for and exit loop if error is found
+   string read;
+   int location = 0;
+   bool error = false;
+   int sizeofcomment = 7;
+
+   while (fin >> read && error == false) {
+     //check for and ignore comments, read until end of comment is found (')')
+     if (read.substr(0, min((int)read.length(), sizeofcomment)) == "comment") {
+       while (read.find("')") == string::npos) {
+         fin >> read;
+       }
+     }
+
+     //check for all the opcodes which increment the location counter by 1
+     else if (
+       read == "iconst_m1" || read == "iconst_0" || read == "iconst_1" ||
+       read == "iconst_2" || read == "iconst_3" || read == "iconst_4" ||
+       read == "iconst_5" || read == "iload_0" || read == "iload_1" ||
+       read == "iload_2" || read == "iload_3" || read == "istore_0" ||
+       read == "istore_1" || read == "istore_2" || read == "istore_3" ||
+       read == "pop" || read == "swap" || read == "dup" || read == "iadd" ||
+       read == "isub" || read == "imul" || read == "idiv" ||
+       read == "irem" || read == "ineg" || read == "ret" ||
+       read == "return")
+       {
+         //increment the location couter by 1
+         location++;
+       }
+       //check for opcodes that have a value with them
+       else {
+         char name[15];
+         char value[15];
+
+         //read through and ignore the parentheses, thing before first
+         //parenthese is the name, second one is the value
+         //ex: bipush(10), bipush is name, 10 is the value
+         if (sscanf(read.c_str(), "%[^(](%[^)])", name, value) != 2) {
+             cerr << "Parentheses Fail" << endl;
+             error = true;
+             return 1;
+         }
+         //check if the name read in is equal to the opcode
+         if (
+           strcmp(name, "bipush") == 0 || strcmp(name, "iload") == 0 ||
+           strcmp(name, "ifeq") == 0 || strcmp(name, "ifge") == 0 ||
+           strcmp(name, "ifgt") == 0 || strcmp(name, "ifle") == 0 ||
+           strcmp(name, "iflt") == 0 || strcmp(name, "ifne") == 0 ||
+           strcmp(name, "goto") == 0 || strcmp(name, "jsr") == 0 ||
+           strcmp(name, "invokevirtual") == 0)
+           //name == "bipush" ||
+           //find(name.begin(), name.end(), "bipush") ||
+           /*name.compare("bipush") == 0 || name.compare("iload") == 0 ||
+           name.compare("ifeq") == 0 || name.compare("ifge") == 0 ||
+           name.compare("ifgt") == 0 || name.compare("ifle") == 0 ||
+           name.compare("iflt") == 0 || name.compare("ifne") == 0 ||
+           name.compare("goto") == 0 || name.compare("jsr") == 0 ||
+           name.compare("invokevirtual") == 0)*/
+           {
+             //increment location counter by 2
+             location+=2;
+           }
+           else if (strcmp(name, "iinc") == 0) {
+             //name.compare("iinc") == 0) {
+             location += 3;
+           }
+           else if (strcmp(name, "label") == 0) {
+             //if label is not in the table already, add it;
+             //if already in the table, give an error
+             if (table.find(value) == table.end()) {
+               table[value] = location;
+             }
+             else {
+               cerr << "Error // Multiply-defined symbols" << value << endl;
+               error = true;
+               return 1;
+             }
+           }
+           //if the name doesnt match a known opcode, give an error
+           else {
+             cerr << "Error // unknown opcode mnemonics" << read << endl;
+             error = true;
+             return 1;
+           }
+        }
+    }
+    //completes pass one
+
+    //reset the file and go back to the start for pass 2
+    fin.clear();
+    fin.seekg(0, ios::beg);
+
+    location = 0;
+    error = false;
+    while (fin >> read && error == false) {
+      if (read.substr(0, min((int)read.length(), sizeofcomment)) == "comment") {
+        while (read.find("')") == string::npos) {
+          fin >> read;
+        }
+      }
+
+      else if (
+        read == "iconst_m1" || read == "iconst_0" || read == "iconst_1" ||
+        read == "iconst_2" || read == "iconst_3" || read == "iconst_4" ||
+        read == "iconst_5" || read == "iload_0" || read == "iload_1" ||
+        read == "iload_2" || read == "iload_3" || read == "istore_0" ||
+        read == "istore_1" || read == "istore_2" || read == "istore_3" ||
+        read == "pop" || read == "swap" || read == "dup" || read == "iadd" ||
+        read == "isub" || read == "imul" || read == "idiv" ||
+        read == "irem" || read == "ineg" || read == "ret" ||
+        read == "return")
+        {
+          executable << location << ": " << opcode[read] << endl;
+          location++;
+        }
+        else {
+          char name[15];
+          char value[15];
+          if (sscanf(read.c_str(), "%[^(](%[^)])", name, value) != 2) {
+              cerr << "Parentheses Fail" << endl;
+              error = true;
+              return 1;
+          }
+
+          if (strcmp(name, "bipush") == 0 || strcmp(name, "invokevirtual") == 0)
+          {
+            executable << location << ": " << opcode[name] << " " <<
+              value << endl;
+            location += 2;
+          }
+          else if (
+            strcmp(name, "iload") == 0 || strcmp(name, "ifeq") == 0 ||
+            strcmp(name, "ifge") == 0 || strcmp(name, "ifgt") == 0 ||
+            strcmp(name, "ifle") == 0 || strcmp(name, "iflt") == 0 ||
+            strcmp(name, "ifne") == 0 || strcmp(name, "goto") == 0 ||
+            strcmp(name, "jsr") == 0)
+            {
+              if (table.find(value) == table.end()) {
+                cerr << "Unknown opcode " << value << endl;
+                error = true;
+                return 1;
+              }
+              else {
+                executable << location << " " << opcode[name] <<
+                " " << table[value] << endl;
+                location+=2;
+              }
+            }
+            else if (strcmp(name, "iinc") == 0) {
+              char firstValue[15];
+              char secondValue[15];
+              if (sscanf(value, "%[^,],%s", firstValue, secondValue) != 2) {
+                  cerr << "Comma Fail (Check Spacing)" << endl;
+                  return 1;
+              }
+              executable << location << ": " << opcode[name] <<
+              " " << firstValue << " " << secondValue << endl;
+              location+=3;
+            }
+        }
+    }
+   return 0;
+ }
